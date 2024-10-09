cd54b99601038dcaaf1aa404293f43500daf1c36
Douglas-Rodrigues1988 <dlorodriguesweb3@gmail.com>
2024-10-09 10:34:10 -0300

    Fim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,58 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+// Verifica se os elementos do DOM existem
+if (!pokemonList || !loadMoreButton) {
+    console.error('Elementos do DOM não encontrados!');
+}
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
-                    
+
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
-                    alt=${pokemon.name}>
+                <img src="${pokemon.photo}" alt="${pokemon.name} - ${pokemon.types.join(', ')}">
             </div>
         </li>
-    `
+    `;
 }
 
-const pokemonList = document.getElementById('pokemonList')
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit)
+        .then((pokemons = []) => {
+            const newHtml = pokemons.map(convertPokemonToLi).join('');
+            pokemonList.innerHTML += newHtml;
+        })
+        .catch((error) => {
+            console.error('Erro ao carregar Pokémon: ', error);
+        });
+}
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonList.innerHTML = newHtml    
-})
+// Carrega os itens iniciais
+loadPokemonItens(offset, limit);
+
+// Adiciona evento ao botão "Load More"
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    const qtdRecordsWithNextPage = offset + limit;
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        // Remove o botão caso todos os Pokémon sejam carregados
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+});
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,59 @@
-const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+const pokeApi = {};
+
+class Pokemon {
+    constructor() {
+        this.number = 0;
+        this.name = '';
+        this.types = [];
+        this.type = '';
+        this.photo = '';
+    }
 }
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types?.map((typeSlot) => typeSlot.type.name) || [];
+    const [type] = types;
+
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(convertPokeApiDetailToPokemon)
+        .catch((error) => {
+            console.error('Fetch error: ', error);
+        });
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => Promise.all(pokemons.map(pokeApi.getPokemonDetail)))
+        .then((pokemonsDetails) => pokemonsDetails)
+        .catch((error) => {
+            console.error('Fetch error: ', error);
+        });
+};
